7ad9f6f0be47bc358daa45d69c0989b73ab32f54
lehuuhlong <lehuuhlong@gmail.com>
2025-03-09 10:26:17 +0700

    Add authentication routes to server configuration

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
 const statisticsRoutes = require('./routes/statisticsRoutes');
+const authRoutes = require('./routes/authRoutes');
 const User = require('./models/User');
 const saveBingoNumber = require('./service/saveBingoNumber');
 const moment = require('moment');
@@ -24,10 +25,13 @@ app.use(cors());
 connectDB();
 app.use(express.json());
 
+// Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/transaction', transactionRoutes);
 app.use('/api/statistics', statisticsRoutes);
 
+// Socket.io
 let users = {};
 let usersBoard = {};
 let calledNumbers = [];
