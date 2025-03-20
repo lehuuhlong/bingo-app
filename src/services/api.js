4fbc0048ea059bc9bc4cc65260dc8d841457dc94
lehuuhlong <lehuuhlong@gmail.com>
2025-03-20 21:21:54 +0700

    style: standardize quotes in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_APP_SERVER_URL + "/api";
+const API_URL = import.meta.env.VITE_APP_SERVER_URL + '/api';
 
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
